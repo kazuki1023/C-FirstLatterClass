146d4d1701c38892ab998a3389b0cf2ffb958197
kazuki1023 <k.keio12569@gmail.com>
2023-01-25 17:43:52 +0900

    値を渡せたけど、最初に入力したものしか反応しない

--- finalReport/finalReport.c
+++ finalReport/finalReport.c
@@ -31,51 +31,37 @@ int main()
   double max_distance = 0;
 
   // 距離を計算して最も遠い点を見つける
-  // for (int i = 0; i < n; i++)
-  // {
-  //   // 点の原点からの距離を計算
-  //   double distance = sqrt(points[i].x * points[i].x + points[i].y * points[i].y);
-  //   if (distance > max_distance)
-  //   {
-  //     max_distance = distance;
-  //     int max_distance_x = points[i].x;
-  //     printf("xの値は(%d)\n", max_distance_x);
-  //     int max_distance_y = points[i].y;
-  //     printf("yの値は(%d)\n", max_distance_y);
-  //   }
-
-  //   // 最大値を与える、x,yを代入
-
-  //     printf("xの値は(%d)\n", points[i].x);
-  //     printf("yの値は(%d)\n", points[i].y);
-
-  //     // この変数に代入できてない
-  //     printf("xの値は(%d)\n", max_distance_x);
-  //     printf("yの値は(%d)\n", max_distance_y);
-  // }
-
-  while(i < n)
+  for (int i = 0; i < n; i++)
   {
+    // (x,y) = (0, 0)からの距離を計算
     double distance = sqrt(points[i].x * points[i].x + points[i].y * points[i].y);
-
     if (distance > max_distance)
     {
       max_distance = distance;
-      const int max_distance_x = points[i].x;
+      int max_distance_x = points[i].x;
       printf("xの値は(%d)\n", max_distance_x);
       int max_distance_y = points[i].y;
       printf("yの値は(%d)\n", max_distance_y);
     }
 
-    printf("xの値は(%d)\n", max_distance_x);
-    printf("yの値は(%d)\n", max_distance_y);
-    i ++;
-  }
+    // 最大値を与える、x,yを出力
+    // この変数には代入できてる
+    // printf("xの値は(%d)\n", points[i].x);
+    // printf("yの値は(%d)\n", points[i].y);
 
+    // この変数に代入できてない(意味のわからない値が代入されている)
+    // printf("xの値は(%d)\n", max_distance_x);
+    // printf("yの値は(%d)\n", max_distance_y);
+  }
+  // ここには代入できている
+  printf("xの値は(%d)\n", points[i].x);
+  printf("yの値は(%d)\n", points[i].y);
+
+  // 原点からの距離を小数点以下切り捨てした
   int max_distance_integer = floor(max_distance);
 
-  printf("最大値のときのxの値は(%d)\n", max_distance_x);
-  printf("最大値のときのyの値は(%d)\n", max_distance_y);
+  printf("最大値のときのxの値は(%d)\n", points[i].x);
+  printf("最大値のときのyの値は(%d)\n", points[i].y);
   // 最も遠い点を出力
   printf("max_distance_integerの中身は(%d)\n", max_distance_integer);
   // printf("The most distant point is (%d, %d)", max_distance_x, max_distance_y);
